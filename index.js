6a8ac5ca471704730c659854f6e1e47dce7b2c1c
Kevin Minutti <minutti70@gmail.com>
2020-03-21 18:09:37 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign(object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+return object
+}
+
+function deleteFromObjectByKey(object, key) {
+
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+
+}
